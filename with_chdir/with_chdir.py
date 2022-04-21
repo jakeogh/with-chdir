1ec163fcf90eb465e71e93b13dae56d277eeeba6
jakeogh <github.com@v6y.net>
2022-04-21 12:14:30 -0700

    auto-commit

--- with_chdir/with_chdir.py
+++ with_chdir/with_chdir.py
@@ -10,13 +10,14 @@ import sh
 from epprint import epprint
 
 
-class chdir():
-    def __init__(self,
-                 path,
-                 *,
-                 verbose: Union[bool, int, float],
-                 unwrite: bool = False,
-                 ):
+class chdir:
+    def __init__(
+        self,
+        path,
+        *,
+        verbose: Union[bool, int, float],
+        unwrite: bool = False,
+    ):
         self.verbose = verbose
         self.orig_path = Path(os.getcwd())
         self.path = Path(os.fsdecode(path)).resolve()
@@ -25,27 +26,28 @@ class chdir():
     def __enter__(self):
         os.chdir(self.path)
         if self.verbose:
-            epprint(f'{self.path=}')
+            epprint(f"{self.path=}")
         if self.unwrite:
-            sh.chmod('+w', '.')
+            sh.chmod("+w", ".")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.unwrite:
-            sh.chmod('-w', '.')
+            sh.chmod("-w", ".")
         os.chdir(self.orig_path)
 
 
 @click.command()
 @click.argument("path", type=str, nargs=1)
-@click.option('--verbose', is_flag=True)
+@click.option("--verbose", is_flag=True)
 @click.pass_context
-def cli(ctx,
-        path: str,
-        verbose: Union[bool, int, float],
-        ):
+def cli(
+    ctx,
+    path: str,
+    verbose: Union[bool, int, float],
+):
 
     with chdir(path, verbose=verbose):
-        os.system('pwd')
+        os.system("pwd")
         os.system("ls -al")
 
-    os.system('pwd')
+    os.system("pwd")
